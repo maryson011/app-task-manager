9eb6dda35dd3e98baba17f610afdd3f4f1b9eaa8
maryson gleison <maryson011@gmail.com>
2024-07-23 21:37:25 -0300

    feat: criado rota para busca de tarefa

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,17 @@ app.get('/tasks', async (req, res) => {
     }
 })
 
+app.get('/tasks/:id', async (req, res) => {
+    try {
+        const task = await TaskModel.findById(req.params.id)
+
+        if (!task) return res.status(404).send("Essa tarefa não foi encontrada!")
+        res.status(200).send(task)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     try {
         const newTask = new TaskModel(req.body)
@@ -36,7 +47,7 @@ app.delete('/tasks/:id', async (req, res) => {
         const taskId = req.params.id
         const taskToDelete = await TaskModel.findById(taskId)
         if (!taskToDelete) {
-            return res.status(500).send("Essa tarefa não foi encontrada!")
+            return res.status(404).send("Essa tarefa não foi encontrada!")
         }
         const deleteTask = await TaskModel.findByIdAndDelete(taskId)
         res.status(200).send(deleteTask)
