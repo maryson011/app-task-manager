0403ea0a3251eb550ceb100342138e0037736f40
maryson gleison <maryson011@gmail.com>
2024-07-23 21:27:36 -0300

    feat: criado rota para deletar tarefa

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,29 @@ app.get('/tasks', async (req, res) => {
 })
 
 app.post('/tasks', async (req, res) => {
-    const newTask = new TaskModel(req.body)
+    try {
+        const newTask = new TaskModel(req.body)
+    
+        await newTask.save()
+    
+        res.status(201).send(newTask)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
+})
 
-    await newTask.save()
-
-    res.status(201).send(newTask)
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id
+        const taskToDelete = await TaskModel.findById(taskId)
+        if (!taskToDelete) {
+            return res.status(500).send("Essa tarefa não foi encontrada!")
+        }
+        const deleteTask = await TaskModel.findByIdAndDelete(taskId)
+        res.status(200).send(deleteTask)
+    } catch (e) {
+        res.status(500).send(e.message)
+    }
 })
 
 app.listen(8000, () => console.log('Listening on port 8000!'))
